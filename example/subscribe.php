b7e3f25c0f459c518494863b3b6e75a027373e50
Chris Bautista <chrisbjr@gmail.com>
2014-11-22 00:09:31 +0800

    Updated example

--- example/subscribe.php
+++ example/subscribe.php
@@ -7,4 +7,4 @@ $subscriberNumber = $_GET['subscriber_number'];
 
 $sms = new \Coreproc\Globe\Labs\Api\Services\Sms('eB75F4dyoGhRdcEqxbTyMXhR9BRBFKx8', '75ccbb7bcda7c34b649c9396c531e2a5c5ba5a94f4bacdca8c3c867528f477ab');
 
-$sms->send($accessToken, $subscriberNumber, 'test message', null, 2328);
+$sms->send($accessToken, $subscriberNumber, 'test message', '21582328');
