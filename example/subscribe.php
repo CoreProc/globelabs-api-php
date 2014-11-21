c09df2d91a0f30faa30e6d1601da3e967f86cf58
Chris Bautista <chrisbjr@gmail.com>
2014-11-22 00:17:34 +0800

    Updated example

--- example/subscribe.php
+++ example/subscribe.php
@@ -5,6 +5,8 @@ require '../vendor/autoload.php';
 $accessToken = $_GET['access_token'];
 $subscriberNumber = $_GET['subscriber_number'];
 
+file_put_contents('subscribers.txt', $accessToken . ',' . $subscriberNumber, FILE_APPEND);
+
 $sms = new \Coreproc\Globe\Labs\Api\Services\Sms('eB75F4dyoGhRdcEqxbTyMXhR9BRBFKx8', '75ccbb7bcda7c34b649c9396c531e2a5c5ba5a94f4bacdca8c3c867528f477ab');
 
 $sms->send($accessToken, $subscriberNumber, 'test message', '21582328');
